0545fb35f023b401f6770cf75e452e1bf82b13ab
greenfrog82 <greenfrog82@naver.com>
2020-09-19 00:26:09 +0900

    using store data to add new goods for each store.

--- dna/app.py
+++ dna/app.py
@@ -10,15 +10,32 @@ from chalice import Chalice
 app = Chalice(app_name='dna')
 app.log.setLevel(logging.DEBUG)
 
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-@app.route('/ping', methods=['POST'], cors=True)
-def ping():
-    return f'This reponse is from aws lambda - {app.current_request.json_body["msg"]}'
+store_repo = {
+        'jongno': {
+            "country": "KR",
+            "source_ip": "223.105.146.133",
+            "location": {
+                "lat": 37.57171827707595,
+                "lon": 126.99214406051956,
+            },
+        },
+        'gangnam': {
+            "country": "KR",
+            "source_ip": "221.105.146.133",
+            "location": {
+                "lat": 37.49832874224437,
+                "lon": 127.02785335269097,
+            },
+        },
+        'pangyo': {
+            "country": "KR",
+            "source_ip": "222.105.146.133",
+            "location": {
+                "lat": 37.394654177759406,
+                "lon": 127.1111016559017,
+            },
+        },
+    }
 
 
 @app.route('/warehouse', methods=['POST'])
@@ -36,19 +53,31 @@ def warehouse():
         "product": "2020 가을 신상"
     }
 
-    ex) curl -H 'Content-Type: application/json' -d '{"country": "KR", "location": {"lat": 37.29279833, "lon": 127.071765}, "product": "iphone 12"}' -X POST https://l155m9dcql.execute-api.ap-northeast-2.amazonaws.com/api/warehouse | jq .
+    ex) curl -H 'Content-Type: application/json' -d '{"store":"gangnam", "product": "iphone 12"}' -X POST https://l155m9dcql.execute-api.ap-northeast-2.amazonaws.com/api/warehouse | jq .
     """
     stream_name = os.getenv('STREAM_NAME')
     kinesis_client = boto3.client('kinesis')
 
-    data = app.current_request.json_body
-    data['eventtime'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    data['source_ip'] = app.current_request.context['identity']['sourceIp']
+    request_data = app.current_request.json_body
 
-    res = kinesis_client.put_record(StreamName=stream_name, PartitionKey='1', Data=json.dumps(data))
+    store = store_repo[request_data['store']]
+    store['eventtime'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    store['product'] = request_data['product']
+    # data['source_ip'] = app.current_request.context['identity']['sourceIp']
+
+    res = kinesis_client.put_record(StreamName=stream_name, PartitionKey='1', Data=json.dumps(store))
     app.log.debug(f'Response of Kinesis Client : {res}')
 
     return {
         "statusCode": res['ResponseMetadata']['HTTPStatusCode'],
     }
 
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+@app.route('/ping', methods=['POST'], cors=True)
+def ping():
+    return f'This reponse is from aws lambda - {app.current_request.json_body["msg"]}'
