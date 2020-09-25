1141ad09de93cbb1b125f7876af88db3dfd7a762
greenfrog82 <greenfrog82@naver.com>
2020-09-25 19:27:03 +0900

    [CDK] add S3 and Cloudformation

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,8 +1,9 @@
 from aws_cdk import (
     core, 
     aws_s3 as s3, 
-    aws_cloudfront as cfn, 
-    aws_cloudfront_origins as origins, 
+    aws_cloudfront as cfn,
+    aws_iam as iam,
+    # aws_cloudfront_origins as origins, 
 )
 
 
@@ -12,7 +13,26 @@ class CdkStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
-        bucket = s3.Bucket(self, 'dna-front', versioned=True,)
-        cfn.Distribution(self, 'dna-front-deploy',
-            default_behavior=cfn.BehaviorOptions(origin=origins.S3Origin(bucket))
+        source_bucket = s3.Bucket(self, 'DnaFront', versioned=True,)
+        source_bucket.add_to_resource_policy(iam.PolicyStatement(
+            actions=["s3:GetObject"],
+            resources=[source_bucket.arn_for_objects("*")],
+            principals=[iam.AnyPrincipal()],
+        ))
+
+        distribution = cfn.CloudFrontWebDistribution(self, "DnaFrontEndDistributor",
+            origin_configs=[
+                cfn.SourceConfiguration(
+                    s3_origin_source=cfn.S3OriginConfig(
+                        s3_bucket_source=source_bucket
+                    ),
+                    behaviors=[cfn.Behavior(is_default_behavior=True)]
+                )
+            ],
+            default_root_object='index.html',
+            viewer_protocol_policy=cfn.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            error_configurations=[
+                cfn.CfnDistribution.CustomErrorResponseProperty(error_code=403, response_code=200, error_caching_min_ttl=5, response_page_path='/index.html'),
+                cfn.CfnDistribution.CustomErrorResponseProperty(error_code=404, response_code=200, error_caching_min_ttl=5, response_page_path='/index.html'),
+            ],
         )
